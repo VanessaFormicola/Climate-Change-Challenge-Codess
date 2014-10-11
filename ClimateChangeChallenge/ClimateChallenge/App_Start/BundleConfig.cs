fc806741f7b6b55d545f2f52ed461072262d43ce
Francesca0309 <francesca.ycnarf@yahoo.com>
2014-10-11 13:49:07 +0100

    Test_dataset display on alert

--- ClimateChangeChallenge/ClimateChallenge/App_Start/BundleConfig.cs
+++ ClimateChangeChallenge/ClimateChallenge/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace ClimateChallenge
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.csv-0.71.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
